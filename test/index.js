a763a5d7a02f35bac17e7f0f0f520b68f53f7878
AtuyL <atuyl.jp@gmail.com>
2014-09-21 03:58:06 +0900

    changed test-framework. use the `lab`. added: Measures for deleted sprite(png).

--- test/index.js
+++ test/index.js
@@ -1,80 +1,78 @@
-var fs = require('fs'),
-    http = require('http'),
-    express = require('express'),
-    stylus = require('stylus'),
-    stylsprite = require('../lib'),
-    arrayUtils = require('../lib/utils/array'),
-    async = require('async'),
-    join = require('path').join,
-    relative = require('path').relative,
-    tests = [];
+'use strict';
 
-tests.push(function (next) {
-    var cases = [null, [1], 2, [[3, 4], 5], [[[]]], [[[6]]], 7, 8, [], null];
-    console.log(cases.reduce(arrayUtils.flatten));
-    return next();
-});
+var chalk = require('chalk'),
 
-tests.push(function (next) {
-    var eachRequest, index, options, requests, stylsprite_mw, stylus_mw, i;
-    options = {
-        src: 'test/src/stylus',
-        dest: 'test/public/css',
-        root: 'test/public',
-        imgsrc: 'test/src/imgsrc'
-    };
-    stylsprite_mw = stylsprite.middleware(options);
-    options.compile = stylsprite.middleware.compile;
-    options.compile = function (str, path) {
-        var context, dest, destFile, root, src;
-        dest = this.dest, src = this.src, root = this.root;
-        context = stylus(str);
-        destFile = join(dest, relative(src, path).replace(/\.styl$/, '.css'));
-        context.define('stylsprite', stylsprite(destFile, root));
-        return context;
-    };
-    stylus_mw = stylus.middleware(options);
-    requests = [];
-    for (index = i = 0; i < 5; index = ++i) {
-        requests.push({
-            method: 'GET',
-            url: '/hoge.css'
+    Lab = require('lab'),
+    lab = exports.lab = Lab.script(),
+
+    suite = lab.suite,
+    test = lab.test,
+    before = lab.before,
+    after = lab.after,
+    expect = Lab.expect;
+
+lab.experiment('utils/array', function () {
+    var array = require('../lib/utils/array'),
+        flatten = array.flatten,
+        uniq = array.uniq;
+    lab.test('flatten', function (done) {
+        var cases = [null, [1], 2, [
+                [3, 4], 5
+            ],
+            [
+                [
+                    []
+                ]
+            ],
+            [
+                [
+                    [6]
+                ]
+            ], 7, 8, [], null
+        ];
+        cases = flatten(cases);
+        expect(cases.join(',')).to.equal(',1,2,3,4,5,6,7,8,');
+        done();
+    });
+    lab.test('uniq', function (done) {
+        var cases = ['a', 'b', 'a', null, undefined, null, undefined, NaN],
+            equal = ['a', 'b', null, undefined],
+            result;
+        cases = uniq(cases);
+        result = cases.every(function (value, index) {
+            return equal[index] === value;
         });
-        requests.push({
-            method: 'GET',
-            url: '/fuga.css'
-        });
-    }
-    eachRequest = function (req, next) {
-        return stylsprite_mw(req, {}, function () {
-            return stylus_mw(req, {}, next);
-        });
-    };
-    return async.each(requests, eachRequest, function () {
-        return next();
+        expect(result).to.equal(true);
+        done();
     });
 });
 
-async.series(tests, function (error) {
-    var app, options;
-    if (error) {
-        return console.log(error);
-    }
-    app = express();
-    app.set('views', __dirname + '/src');
-    app.set('view engine', 'jade');
-    options = {
-        src: 'test/src/stylus',
-        dest: 'test/public/css',
-        root: 'test/public',
-        imgsrc: 'test/src/imgsrc'
-    };
-    app.use('/css', stylsprite.middleware(options));
-    options.compile = stylsprite.middleware.compile;
-    app.use('/css', stylus.middleware(options));
-    app.use(express["static"](__dirname + '/public'));
-    app.get('/', function (req, res) {
-        return res.render('index');
+lab.experiment('middleware', function () {
+    var stylus = require('stylus'),
+        stylsprite = require('../lib'),
+        options = stylsprite.middleware.options(
+            'test/src/stylus',
+            'test/public/css',
+            'test/public',
+            'test/src/imgsrc'
+        ),
+        stylsprite_mw = stylsprite.middleware(options),
+        stylus_mw = stylus.middleware(options);
+
+    lab.test('dummy requests', function (done) {
+        var async = require('async'),
+            i,
+            requests = ['/hoge.css','/fuga.css'];
+        requests = requests.map(function(cssPath){
+            return {
+                method: 'GET',
+                url: cssPath
+            }
+        });
+        return async.each(requests, function (req, next) {
+            return stylsprite_mw(req, {}, function () {
+                return stylus_mw(req, {}, next);
+            });
+        }, done);
     });
-    return app.listen(8080);
 });
